5dacc5822301af6c9295b912a463d520dc779006
jaybeylic <sakayusuf8@gmail.com>
2023-03-19 11:22:59 +0100

    update

--- v1/views/mail.php
+++ v1/views/mail.php
@@ -6,7 +6,7 @@ if($_SERVER['REQUEST_METHOD'] !=="POST"){
    die;
 }
 
-if( !in_array($request_headers['Host'],["www.astrocybernet.com"]) ){
+if( !in_array($request_headers['Host'],["https://astrocybernet.com"]) ){
   http_response_code(502);
   die;
 }
